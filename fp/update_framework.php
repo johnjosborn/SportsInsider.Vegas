7059720c10f75aba0c2684e1638652fd1febac0b
John <john@lab627.com>
2017-11-07 19:53:25 -0800

    Twitter mock done

--- fp/update_framework.php
+++ fp/update_framework.php
@@ -1,15 +1,15 @@
 <?php
 
-if (isset($_POST['frame_no'])){
+// if (isset($_POST['frame_no'])){
 
-    $frameNum = $_POST['frame_no'];
+//     $frameNum = $_POST['frame_no'];
 
-} else {
+// } else {
 
-    $frameNum = "2";
-}
+//     $frameNum = "2";
+// }
 
-$frameNum = "2";
+$frameNum = "3";
 
 switch ($frameNum) {
     case '1':
@@ -85,6 +85,61 @@ switch ($frameNum) {
             
         ";
         break;
+
+        case '3':
+        
+            echo "<br><br>
+                <div class='catch breaking'>[Select Tag]</div>
+                <br>
+                <div class='headline'>
+                    <span class='ply1'>[Select Player]</span>&nbsp
+                    (<span class='inj4'>[Select Injury]</span>)&nbsp
+                    <span class='dur1'>[Select Severity]</span>
+                </div>
+                <br>
+                <div id='byLineCont'>
+                    <img id='byPhotoImg' src=''>
+                    <span class='byl1 inline'>[Select Writer]</span>&nbsp|&nbsp<span class='byl3 inline'>[Select Writer Job]</span>
+                </div>
+                <div class='addthis_inline_follow_toolbox'></div>
+                <div class='posting'>
+                    <span class='post1'>Submitted October 5, 2017  5:00PM</span>
+                    <br>
+                    <span class='post2'>Updated October 5, 2017  5:00PM</span>
+                </div>
+                <br>
+                <div id='articleText'>
+                    <span class='ply6 cityHead'>[Select Player]</span>&nbsp-&nbspSources inside the
+                    <span class='ply3'>[Select Player]</span>&nbsporganization have stated that
+                    <span class='ply5'>[Select Player]</span>&nbspstar
+                    <span class='ply2'>[Select Player]</span>
+                    <span class='dur2'>[Select Duration]</span>&nbspdue to
+                    <span class='inj1'>[Select Injury]</span>.
+                    <br>
+                    <img id='injuryImg' src=''>
+                    <br>
+                    <span class='qte1'>[Select Quote]</span>
+                </div>
+                <div style='clear: both'></div>
+                <div id='tweet'>
+                    <table>
+                        <tr>
+                            <td><img src='media/twt/twtUsr.png'></td>
+                            <td><span class='twtTitle'>Vegas Sports Insider</span><img src='media/twt/ver.jpg' class='twtIcon'>
+                            <br><span class='twtSub'>@VegasSportsInsider</span></td>
+                        </tr>
+                    </table>
+                    <br>
+                    Condolences to the fantasy owners who were banking on <span class='ply3'>[Select Player]</span>
+                    &nbsp<span class='ply2'>[Select Player]</span>
+                    .  He <span class='dur2'>[Select Duration]</span><br>
+                    <div id='twtBird'><img src='media/twt/brd.png' class='twtIcon'></div>   
+                    <span class='twtSub'>10:42AM - Nov 7, 2017</span><br>
+                    <img src='media/twt/tweetsub.png' class='twtStats'>            
+                </div>
+                
+            ";
+            break;
     
     default:
         # code...
